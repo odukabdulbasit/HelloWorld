aa0bf1a6ae618f396a8c1ff9db4ac25336c2bc53
Abdulbasit Oduk <a.oduk@pegb.tech>
2023-08-07 16:07:39 +0300

    Added Log message

--- app/src/main/java/com/odukabdulbasit/helloworld/MainActivity.kt
+++ app/src/main/java/com/odukabdulbasit/helloworld/MainActivity.kt
@@ -2,10 +2,18 @@ package com.odukabdulbasit.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //You can check this result by clicking Logcat tab in bottom menu
+        helloWorld()
+    }
+
+    private fun helloWorld() {
+        Log.i("message", "Hello World!")
     }
 }
